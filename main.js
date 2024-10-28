7fa3b4bc15b9b9604640af24882a8d4e53d983df
MacacoRei04 <163008169+MacacoRei04@users.noreply.github.com>
2024-10-28 15:35:59 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    // Exemplo de funcionalidade para carregar notícias
+    const noticias = document.getElementById("noticias");
+    noticias.innerHTML += "<p>Nova notícia: Palmeiras campeão!</p>";
+  });
